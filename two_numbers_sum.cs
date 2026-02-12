6976705ec8b417b2e84206f3f3bc8453fb27b340
Muhammad Ayaan Naeem <muhammadayaan6128@gmail.com>
2026-02-13 00:31:30 +0500

    Add files via upload

---
+++ two_numbers_sum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a;
+            int b;
+            int sum;
+
+            Console.Write("Enter the first number: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.Write("Enter the second number: ");
+            b = int.Parse(Console.ReadLine());
+
+            sum = a + b;
+
+            Console.WriteLine($"The sum will be: {sum}");
+
+        }
+    }
+}
